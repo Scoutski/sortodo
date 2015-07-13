8fd01ec4f78fe80164ba5dd5aff856eb0c9efa0a
mcooper <mikeyjc@gmail.com>
2015-07-13 18:27:08 +1000

    added priority column to tasks column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150713051412) do
+ActiveRecord::Schema.define(version: 20150713082616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20150713051412) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.integer  "priority"
   end
 
   create_table "users", force: :cascade do |t|
