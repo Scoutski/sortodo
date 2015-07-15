fe8d6f15596f0c20ce6111c384616e0e8f9daf16
mcooper <mikeyjc@gmail.com>
2015-07-15 20:12:51 +1000

    setup the notebooks div to look a bit prettier

--- app/assets/javascripts/backbone/views/notebookListView.js
+++ app/assets/javascripts/backbone/views/notebookListView.js
@@ -3,16 +3,34 @@
 var app = app || {};
 
 app.NotebookListView = Backbone.View.extend({
-  tagName: 'p',
+  tagName: 'div',
 
   events: {
     'click': 'showTasks'
   },
 
   render: function() {
+    var model = this.model.toJSON();
     var notebookListTemplate = $('#notebookListTemplate').html();
     var notebookListHTML = _.template(notebookListTemplate);
-    var toAppend = this.$el.html(notebookListHTML(this.model.toJSON()) );
+    var bg = hexToRGB(model['color']);
+    var fadedColor = 'rgba(' + bg['r'] + ',' + bg['g'] + ',' + bg['b'] + ',0.4)';
+    var fullColor = 'rgba(' + bg['r'] + ',' + bg['g'] + ',' + bg['b'] + ',1)';
+
+    this.$el.css('background-color', fadedColor);
+    
+    var toAppend = this.$el.html(notebookListHTML(model) );
+    
+
+    this.$el.mouseenter(function() {
+      $(this).css("background-color", fullColor)
+      .css("-webkit-transition", ".4s")
+      .css('transition', '.4s');
+    }).mouseleave(function() {
+      $(this).css('background-color', fadedColor)
+      .css('-webkit-transition', '.4s')
+      .css('transition', '.4s');
+    });
     $('#notebook').append(toAppend);
   },
 
@@ -21,3 +39,12 @@ app.NotebookListView = Backbone.View.extend({
     app.router.navigate('notebooks/' + this.model.get('id') + '/tasks', true);
   }
 });
+
+var hexToRGB = function (hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+};
